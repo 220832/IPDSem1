2d69ba6dbe3b21dffe65cb348817ce5063ca0507
CHartman <57812431+220832@users.noreply.github.com>
2019-11-18 14:56:29 -0600

    Update team13.py

--- team13.py
+++ team13.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Calvin 13' # Only 10 chars displayed.
+strategy_name = 'betray 7th turn passive after 3'
+strategy_description = 'Collude until bertayed if 3 coludes in a row start over every 7 turns betray no matter what if my score is lower betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
