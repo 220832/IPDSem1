b04b4f6791bf055121cd830e808404b4d0226192
brkoivisto <240655@shakopeeschools.org>
2019-11-21 14:58:38 -0600

    Final Strat

--- team4.py
+++ team4.py
@@ -20,17 +20,36 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if len(my_history) == 0:
+        return 'c'
+        
+    if 'b' in their_history[-3:-1]:
+        return 'b'
+        
+    if 'b' not in their_history:
+        return 'c'
+        
+    if 'cbc' in their_history[-3:-1]:
+        return 'b'
+        
+    if 'bcb' in their_history[-3:-1]:
+        return 'c'
+        
+    if 'c' in their_history[-2:-1]:
+        return 'c'
+        
+        
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
